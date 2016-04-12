fbf9f38b6eeb9907c0bdced4e8f3936efeda1ad8
Andrew Green <andrew.green.df@gmail.com>
2016-04-12 13:12:21 -0400

    kvStoreMaintenance: Refactor to use requestIdleCallback (PS12)

--- resources/subscribing/ext.centralNotice.kvStoreMaintenance.js
+++ resources/subscribing/ext.centralNotice.kvStoreMaintenance.js
@@ -15,26 +15,68 @@
 
 		// Time past expiry before actually removing items: 1 day (in seconds).
 		// (This should prevent race conditions among browser tabs.)
-		LEEWAY_FOR_REMOVAL = 86400,
+		LEEWAY_FOR_REMOVAL = 86400;
 
-		// Maximum number of keys to process at a time.
-		MAX_BATCH_SIZE = 10,
+	function makeRemoveExpiredFunction( length, d ) {
+		// We don't know how far we'll get before the user navigates away. Start at a random index
+		// and wrap around. This makes checking all items eventually more likely, even if there
+		// are many keys and/or each page view is very short.
+		var start = Math.floor( Math.random() * length ),
+			wrapped = false,
+			i = null;
+		function next() {
+			if ( i === null ) {
+				i = start;
+			} else {
+				// Iterate backwards because removing items changes the key index. Otherwise we'd
+				// consistently miss keys. Example:
+				//
+				//     keys = [ foo, bar, baz ]
+				//     start = i = random() // 1
+				//     remove( i );
+				//     keys = [ foo, baz ]
+				//
+				// To avoid an infinite loop, we stop if we're back at the start. But, the start
+				// must be moved along after each removal as otherwise we'd still some keys.
+				//
+				//     keys = [ foo, bar, baz, quux ]
+				//     start = i = random() // 1
+				//     remove( from i to 0 )
+				//     keys = [ baz, quux ]
+				//     i = length - 1 // Wrap around
+				//     remove( from i to 1 )
+				//     keys = [ baz ] // missed!
+				//
+				// Note: This is by no means guruanteed since users can close the page, and keys
+				// may be missed due to races with other tabs. But those have their own removal
+				// process at least and that's why we start at a random index.
+				i--;
+				if ( i < 0 ) {
+					// Wrap around, continue from the end
+					i = length - 1;
+					wrapped = true;
+				}
+				if ( wrapped && ( i === start || i <= 0 ) ) {
+					// Complete
+					i = null;
+				}
+			}
+			return i;
+		}
+		return function removeExpired( deadline ) {
+			var index, key, rawValue, value;
+			while ( deadline.timeRemaining() > 5 ) {
+				index = next();
+				if ( index === null ) {
+					// Reached end
+					break;
+				}
+				key = localStorage.key( index );
 
-		// Maximum number of items to process on any given pageview.
-		// This ensures we don't block the browser too much when we prepare the
-		// arrays of keys and batch functions.
-		MAX_ITEMS_TO_PROCESS = 60;
-
-	function makeRemoveExpiredBatchFunction( keys ) {
-		return function() {
-
-			var n, key, rawValue, value;
-
-			for ( n = 0; n < keys.length; n++ ) {
-				key = keys[n];
-
-				// Operate only on localStorage items used by the kvStore
-				if ( !PREFIX_REGEX.test( key ) ) {
+				// Recheck key existence. While JS execution is single-threaded, storage is
+				// shared across multiple browsing contexts. It may race with other tabs.
+				// Operate only on localStorage items used by the kvStore.
+				if ( key === null || !PREFIX_REGEX.test( key ) ) {
 					continue;
 				}
 
@@ -44,7 +86,7 @@
 					return;
 				}
 
-				// The item might have been removed since we retrieved the key
+				// The item might have been removed already
 				if ( rawValue === null ) {
 					continue;
 				}
@@ -54,6 +96,9 @@
 				} catch ( e ) {
 					// Remove any unparseable items and maybe set an error
 					localStorage.removeItem( key );
+					if ( !wrapped ) {
+						start--;
+					}
 
 					if ( cn.kvStore ) {
 						cn.kvStore.setMaintenanceError( key );
@@ -66,7 +111,16 @@
 					( value.expiry + LEEWAY_FOR_REMOVAL ) < now ) {
 
 					localStorage.removeItem( key );
+					if ( !wrapped ) {
+						start--;
+					}
 				}
+			}
+			if ( index !== null ) {
+				// Time's up, continue later
+				mw.requestIdleCallback( removeExpired );
+			} else {
+				d.resolve();
 			}
 		};
 	}
@@ -80,62 +134,24 @@
 	cn.kvStoreMaintenance = {
 
 		/**
-		 * Schedule the batched removal of expired KVStore items.
+		 * Schedule the removal of expired KVStore items.
+		 *
+		 * @return {jQuery.Promise}
 		 */
 		removeExpiredItemsWhenIdle: function () {
-
-			var funcs, keys, i, stopBefore, key,
-				j = 0,
-				keysToProcess;
-
+			var d = $.Deferred();
 			try {
-				if ( !window.localStorage || localStorage.length === 0) {
-					return;
+				if ( !window.localStorage || !localStorage.length ) {
+					return d.resolve();
 				}
 			} catch ( e ) {
-				return;
+				return d.resolve();
 			}
 
-			// We don't know how many batches we'll get through before the user
-			// navigates away, and there may be more localStorage items than
-			// MAX_ITEMS_TO_PROCESS. So we choose a random key index to start
-			// at, and wrap around until we've collected all keys, or the
-			// maximum number.
-			// This way we're likely to get to all items eventually, even if
-			// there are a lot of them and/or each pageview is very quick.
-			i = Math.floor( Math.random() * localStorage.length );
-			stopBefore =
-				( i + Math.min( MAX_ITEMS_TO_PROCESS, localStorage.length ) )
-				% localStorage.length;
+			// Schedule
+			mw.requestIdleCallback( makeRemoveExpiredFunction( localStorage.length, d ) );
 
-			// Build an array of localStorage keys
-			keys = [];
-			do {
-				key = localStorage.key( i );
-
-				// Don't assume that the number of keys hasn't changed and that
-				// the key exists.
-				if ( key !== null ) {
-					keys.push( key );
-				}
-
-				i++;
-				if ( i === localStorage.length ) {
-					i = i - localStorage.length;
-				}
-			} while ( i !== stopBefore );
-
-			// Build an array of functions to process the keys in batches
-			funcs = [];
-			while ( j < keys.length ) {
-				keysToProcess = keys.slice( j,
-					Math.min( keys.length, j + MAX_BATCH_SIZE ) );
-
-				funcs.push( makeRemoveExpiredBatchFunction( keysToProcess ) );
-				j += MAX_BATCH_SIZE;
-			}
-
-			cn.doIdleWork( funcs );
+			return d.promise();
 		}
 	};
 
